d87dbfb718d553a9f4ddb0637c7cf5e9cc3de875
Zhongbo Li <zli73@vols.utk.edu>
2017-10-27 13:44:43 -0400

    Add files via upload

---
+++ cbc_enc.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python2
+import binascii
+from Crypto.Cipher import AES
+from binascii import *
+from Crypto import Random
+import sys
+
+#padding generation
+
+
+
+def padding(inputfile):
+    # inputdata = open(inputfile)
+    textblock = []
+    flag = 0
+    index = 0
+    while index < len(inputfile):
+        if index + 16 > len(inputfile):
+            textblock.append(inputfile[index:])
+            flag = 1
+            break
+        if index + 16 == len(inputfile):
+            textblock.append(inputfile[index:])
+            break
+        # print inputfile[index:16]
+        textblock.append(inputfile[index: index + 16])
+        index += 16
+
+    if flag == 1:
+        numtopad = 16 - len(textblock[-1])
+        lastblock = textblock[-1]
+        padding = ""
+        for _ in xrange(numtopad):
+            padding += str("0"+hex(numtopad)[-1])
+        lastblock += padding.decode("hex")
+        textblock[-1] = lastblock
+    else:
+        lastblock = "10101010101010101010101010101010".decode("hex")
+        textblock.append(lastblock)
+
+    paddedstring = ""
+    for block in textblock:
+        paddedstring += block
+    return paddedstring
+
+def Xoring(a,b):
+    c=int(a,16)^int(b,16)
+    c=format(c, '#034x')[2:]
+    return c
+
+def ECBencrypt(key, raw):
+    if (raw is None) or (len(raw) == 0):
+        raise ValueError('input text cannot be null or empty set')
+    cipher = AES.AESCipher(key[:32], AES.MODE_ECB)
+    ciphertext = cipher.encrypt(raw)
+    return binascii.hexlify(bytearray(ciphertext)).decode('utf-8')
+
+
+
+def CBC_ENC(inputF,IV,k,BS):
+    ct=''
+    msg_pad = padding(inputF)
+    for i in range(0,len(msg_pad)/BS):
+        msg_blocksize=hexlify(msg_pad[BS*i:BS*(i+1)])
+        d=unhexlify(Xoring(msg_blocksize,IV))
+        ci=ECBencrypt(k,d)
+        ct= ct+ci
+        IV=ci
+    return ct
+
+if __name__ == "__main__":
+    f_key= open('keyFile2','r')
+    KEY=f_key.read()
+    f_input = open('testFile1', 'r' )
+    inputFile=f_input.read()
+    f_IV = open('IV','r')
+    IV = f_IV.read()
+    print IV
+    BS = AES.block_size
+    cipher=CBC_ENC(inputFile,IV,KEY,BS)
+    print cipher
+    f_cipher= open('outPutfile_cbc','wb')
+    f_cipher.write(cipher)
+    f_cipher.close()
+
+
---
+++ ctr_dec.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python2
+import binascii
+from binascii import *
+from Crypto.Cipher import AES
+from Crypto import Random
+from multiprocessing import *
+import string
+
+
+
+def Xoring(a,b):
+    print a
+    print b
+    c=int(a,16)^int(b,16)
+    c=format(c,'#034x')[2:]
+    return c
+
+def ECBencrypt(key, raw):
+    if (raw is None) or (len(raw) == 0):
+        raise ValueError('input text cannot be null or empty set')
+    cipher = AES.AESCipher(key[:32], AES.MODE_ECB)
+    ciphertext = cipher.encrypt(raw)
+    return binascii.hexlify(bytearray(ciphertext)).decode('utf-8')
+
+def ctr_enc(key,IV_ctr,enc):
+    init_i= ECBencrypt(key,unhexlify(IV_ctr))
+    pi = Xoring(init_i,enc)
+    #print pi
+    pi=unhexlify(pi)
+    return pi
+
+def ctr_enc_helper(x):
+    return ctr_enc(x[0],x[1],x[2])
+
+
+
+if   __name__ == "__main__":
+    BS = 32
+    f_key = open('KeyFile1', 'r')
+    KEY = f_key.read()
+    f_iv = open('IV','r')
+    iv = f_iv.read()
+    f_enc=open('outPutfile_ctr','r')
+    enc = f_enc.read()
+    if len(enc) % 16 == 0:
+        nb = len(enc) / BS
+    else:
+        nb=len(enc)/BS+1
+    print nb
+    arg_list=[[0,0,0]]
+    for i in range(0,nb-1):
+        arg_list.append([0,0,0])
+    for i in range (0,nb):
+        arg_list[i][0]=KEY
+        arg_list[i][1] = format(int(iv, 16) + 1,'#034x')[2:]
+        arg_list[i][2]= enc[BS*i:BS*(i+1)]
+
+    print arg_list
+
+    p= Pool(4)
+    pi =p.map(ctr_enc_helper,arg_list)
+    print pi
+    pt=''
+    for i in range (0,nb):
+       pt=pt+pi[i]
+    print pt
+    text = open("text_ctr", "wb+")
+    text.write(pt)
+    text.close()
+
+
+
+
+
+
+
+
+
+
+
+
